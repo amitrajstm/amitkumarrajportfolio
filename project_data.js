6e1369bdec94bb6b33aebe52c73c7ff08e9134cc
Amit Kumar Raj <amitraj70913@gmail.com>
2025-02-20 19:46:03 +0530

    Update project_data.js

--- project_data.js
+++ project_data.js
@@ -6,7 +6,7 @@ export const project_data = [
             "The Online Auction System is a web application where users can bid on items in real-time. Sellers can list their items with details like name, description, price, and photos. Buyers can place bids on these items until the auction ends. The highest bid at the end of the auction wins the item. The system includes features like user registration, bidding history, and countdown timers for each auction.",
         badges: ["React js", "Tailwind css", "Express js", "MongoDB", "Redux"],
         github:
-            "https://github.com/theavnishkumar/online-auction-system",
+            "https://github.com/amitrajstm",
         live: "https://auction.kipa.me",
     },
     {
@@ -16,7 +16,7 @@ export const project_data = [
             "Personal Financial Management App is a user-friendly application built with React, Tailwind CSS, Express, and MongoDB. It allows users to create accounts, log in securely, and manage their expenses by adding, deleting, and modifying entries. Users can filter expenses by date, month, or year, and visualize their spending with a pie chart, making financial tracking simple and intuitive.",
         badges: ["React js", "Tailwind css", "Express js", "MongoDB"],
         github:
-            "https://github.com/theavnishkumar/personal-finance-management-app",
+            "https://github.com/amitrajstm",
         live: "https://money.kipa.me",
     },
     {
@@ -25,7 +25,7 @@ export const project_data = [
         description:
             "The URL Shortener Project is a user-friendly web application built on Express.js, EJS, and MongoDB. It simplifies lengthy URLs into shareable, shortened versions, while offering user authentication for signup and login. Users can manage their shortened URLs with ease, and MongoDB integration ensures secure storage of data.",
         badges: ["EJS", "Express js", "MongoDB"],
-        github: "https://github.com/theavnishkumar/url-Shortener",
+        github: "https://github.com/amitrajstm",
         live: "https://url.ihavetech.com",
     },
     {
@@ -34,7 +34,7 @@ export const project_data = [
         description:
             "I created a Library Management System using Java, NetBeans, and MySQL. It's like having a virtual librarian to keep track of books, users, and lending activities. With a user-friendly interface designed in NetBeans, navigating through the system feels intuitive and smooth. Behind the scenes, MySQL ensures that all library data is safely stored and easily accessible. ",
         badges: ["JAVA", "MySql", "Netbeans"],
-        github: "https://github.com/theavnishkumar/LibraryManagement",
+        github: "https://github.com/amitrajstm",
         live: "https://github.com/theavnishkumar/LibraryManagement",
     },
 ];
